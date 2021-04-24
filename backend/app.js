8d8703d695df16c50c5906d8e58526a0f7047589
okwuose ehiedu godstime <ehizeex@okwuoses-MacBook-Pro.local>
2021-04-24 18:45:59 +0100

    fix app and server js config issue

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const path = require('path')
 //setting up config file
 // dotenv.config({path: 'backend/config/config.env'})
 //setting up config file
-if(process.env.NODE_ENV === 'PRODUCTION')require('dotenv').dotenv.config({path: 'backend/config/config.env'})
+if(process.env.NODE_ENV !== 'PRODUCTION')require('dotenv').config({path: 'backend/config/config.env'})
 
 
 
